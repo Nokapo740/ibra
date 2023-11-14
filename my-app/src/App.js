aac6b0263b08842e3d9a13257134910f0b5eff46
Nokapo740 <nokapo740@gmail.com>
2023-11-14 16:52:56 +0600

    ibra

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1 className='heading'>Ассалямуалейкум, я Ибрахим!👋</h1>
+    <h2 className='text'>Это мое 100500-ое приложение на React!</h2>
+    <div className='qqq'></div>
+    </div>
+  );
+}
+
+export default App;
